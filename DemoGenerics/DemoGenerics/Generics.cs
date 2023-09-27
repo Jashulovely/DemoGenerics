62090e2ed5f36601fb14836dfed9cfcd4967a89f
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-27 09:06:00 +0530

    [Jashu]Add : Generics Demo.

---
+++ DemoGenerics/DemoGenerics/Generics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    //Generics means general form 
+    //It does not specify a particular type
+    //Generic allow us to a single class or method that can be used for different data types.
+    // <T> with type parameter
+    // type parameter 
+    internal class Generics <T>
+    {
+        //public string data;
+        public T data;
+        public Generics(T data)
+        {
+            this.data = data;
+            Console.WriteLine(this.data);
+        }
+        public void DemoMethod(T data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+    public class Generic
+    {
+        public void GenericMethod<T>(T info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ DemoGenerics/DemoGenerics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int> generics = new Generics<int>(10);
+            Generics<string> genericsString = new Generics<string>("Jashu");
+            Generics<bool> genericsBool = new Generics<bool>(true);
+            Generics<double> genericsDouble = new Generics<double>(10.9);
+            Console.WriteLine();
+            generics.DemoMethod(999);
+            genericsString.DemoMethod("Kumar");
+            genericsBool.DemoMethod(false);
+            genericsDouble.DemoMethod(55.55);
+            Console.WriteLine();
+            Generic generic = new Generic();
+            generic.GenericMethod(5);
+            generic.GenericMethod("Manoj");
+            generic.GenericMethod(true);
+            generic.GenericMethod(77.77);
+            generic.GenericMethod<int>(5);
+            generic.GenericMethod<string>("Manoj");
+            generic.GenericMethod<bool>(true);
+            generic.GenericMethod<double>(77.77);
+            Console.ReadLine();
+        }
+    }
+}
